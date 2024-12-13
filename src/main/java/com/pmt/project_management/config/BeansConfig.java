cca2647e49a66c8d353a3fa50a5f53cfa7866b42
Wael Zantour <waelzantour1@gmail.com>
2024-12-13 14:37:28 +0100

    Update CORS to allow frontend (nginx) access

--- src/main/java/com/pmt/project_management/config/BeansConfig.java
+++ src/main/java/com/pmt/project_management/config/BeansConfig.java
@@ -55,7 +55,7 @@ public class BeansConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:8081"));
         config.setAllowedHeaders(Arrays.asList(HttpHeaders.ORIGIN, HttpHeaders.CONTENT_TYPE, HttpHeaders.ACCEPT, HttpHeaders.AUTHORIZATION));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "PATCH"));
         source.registerCorsConfiguration("/**", config);
